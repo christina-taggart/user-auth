908fb4b7b0daecd9f03134916f345ea43a39d754
Apprentice <apprentice@dbc11.local>
2014-01-29 19:08:10 -0800

    add create account elements

--- source/dummy_users/app/controllers/index.rb
+++ source/dummy_users/app/controllers/index.rb
@@ -2,15 +2,41 @@ enable :sessions
 
 get '/' do
   @logged_in = "Logged in!" if session[:logged_in]
+  @error = session[:error]
   erb :index
 end
 
 post '/secret_page' do
-  if User.authenticate(params)
-    session[:logged_in] = 'true'
+  if session[:logged_in]
     erb :secret_page
   else
-    @error = "Email or password not recognized!"
-    erb :index
+    redirect '/'
+  end
+end
+
+post '/users/new' do
+  new_user = User.new(params)
+  if new_user.valid?
+    new_user.save
+    session[:logged_in] = 'true'
+    redirect "/users/#{new_user.id}"
+  else
+    session[:error] = "Invalid info"
+    redirect '/'
+  end
+end
+
+get '/users/:id' do
+  @user = User.find(params[:id])
+  erb :user
+end
+
+post '/login' do
+  if User.authenticate(params)
+    session[:logged_in] = 'true'
+    redirect '/secret_page'
+  else
+    session[:error] = "Authentication failed."
+    redirect '/'
   end
 end
