f7f99c63399c49a7c1e3ab6c8b8d92fd491ec876
John Olmsted & Ricardo A. Rubio <pair+qsymmachus.rickarubio@devbootcamp.com>
2014-01-29 17:00:01 -0800

    Add #authenticate to user model

--- source/dummy_users/app/models/user.rb
+++ source/dummy_users/app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
   validates :email, uniqueness: true
+
+  def self.authenticate(email, password)
+    if User.where(email: email, password: password).empty?
+      false
+    else
+      true
+    end
+  end
 end
