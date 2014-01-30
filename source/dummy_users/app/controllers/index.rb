ff8a3895ee2c36137a2d8bdb14ccb904cf0aec70
John Olmsted & Ricardo A. Rubio <pair+qsymmachus.rickarubio@devbootcamp.com>
2014-01-29 17:43:40 -0800

    Add login branching to '/awesome_users_club'

--- source/dummy_users/app/controllers/index.rb
+++ source/dummy_users/app/controllers/index.rb
@@ -1,9 +1,13 @@
+enable :sessions
+
 get '/' do
   erb :index
 end
 
 post '/login' do
   if User.authenticate(params[:email], params[:password])
+    session[:email] = params[:email]
+    session[:name] = User.where(email: params[:email]).first.name
     redirect '/awesome_users_club'
   else
     redirect '/'
@@ -11,5 +15,9 @@ post '/login' do
 end
 
 get '/awesome_users_club' do
-  erb :awesome_users_club
+  if session[:email]
+    erb :awesome_users_club
+  else
+    redirect '/'
+  end
 end
