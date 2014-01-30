73817200e17dee6171286dddbb643aa9ecbe3160
John Olmsted & Ricardo A. Rubio <pair+qsymmachus.rickarubio@devbootcamp.com>
2014-01-29 17:22:16 -0800

    Hashed all passwords

--- source/dummy_users/app/models/user.rb
+++ source/dummy_users/app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ActiveRecord::Base
   validates :email, uniqueness: true
+  before_save :encrypt_password
+
+  def encrypt_password
+    self.password = Digest::SHA256.hexdigest "#{self.password}"
+  end
 
   def self.authenticate(email, password)
-    if User.where(email: email, password: password).empty?
-      false
-    else
-      true
-    end
+    !!User.where(email: email, password: Digest::SHA256.hexdigest(password)).first
   end
 end
