d9fe25bd72b6a3c7fde0615f0c8bd1347b05e9c3
John Olmsted & Ricardo A. Rubio <pair+qsymmachus.rickarubio@devbootcamp.com>
2014-01-29 17:33:34 -0800

    Add '/', '/login', and '/awesome_users_club' routes

--- source/dummy_users/app/controllers/index.rb
+++ source/dummy_users/app/controllers/index.rb
@@ -1,4 +1,15 @@
 get '/' do
-  # Look in app/views/index.erb
   erb :index
+end
+
+post '/login' do
+  if User.authenticate(params[:email], params[:password])
+    redirect '/awesome_users_club'
+  else
+    redirect '/'
+  end
+end
+
+get '/awesome_users_club' do
+  erb :awesome_users_club
 end
