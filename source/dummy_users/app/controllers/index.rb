5f69b7fd5052c870ad8f9ae67cd4d26f342cd3a5
John Olmsted & Ricardo A. Rubio <pair+qsymmachus.rickarubio@devbootcamp.com>
2014-01-29 17:52:46 -0800

    Add logout route

--- source/dummy_users/app/controllers/index.rb
+++ source/dummy_users/app/controllers/index.rb
@@ -6,6 +6,7 @@ end
 
 post '/login' do
   if User.authenticate(params[:email], params[:password])
+    params.delete(:password)
     session[:email] = params[:email]
     session[:name] = User.where(email: params[:email]).first.name
     redirect '/awesome_users_club'
@@ -15,9 +16,16 @@ post '/login' do
 end
 
 get '/awesome_users_club' do
+  p session
   if session[:email]
     erb :awesome_users_club
   else
     redirect '/'
   end
+end
+
+post '/awesome_users_club' do
+  session.each { |key, value| session.delete(key) }
+  p session
+  redirect '/'
 end
