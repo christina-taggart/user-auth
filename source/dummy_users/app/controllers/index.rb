3731bcd31299b8c9798adebad5fd1395b2a7e8be
Apprentice <apprentice@dbc11.local>
2014-01-29 17:34:49 -0800

    add user login and secret page

--- source/dummy_users/app/controllers/index.rb
+++ source/dummy_users/app/controllers/index.rb
@@ -1,3 +1,16 @@
+enable :sessions
+
 get '/' do
+  @logged_in = "Logged in!" if session[:logged_in]
   erb :index
+end
+
+post '/secret_page' do
+  if User.authenticate(params)
+    session[:logged_in] = 'true'
+    erb :secret_page
+  else
+    @error = "Email or password not recognized!"
+    erb :index
+  end
 end
--- source/dummy_users/app/models/user.rb
+++ source/dummy_users/app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   validates :email, uniqueness: true
+
+  def self.authenticate(params)
+    user = User.where(email: params[:email]).first
+    user.password == params[:password]
+  end
 end
